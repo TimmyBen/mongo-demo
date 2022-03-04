76d04c30cbf926d1515205424f31f520e3b2e590
Timi Adewumi <nucleuscaesar@yahoo.com>
2022-03-04 13:14:52 +0100

    Plain Find Comparison Operators

--- index.js
+++ index.js
@@ -32,7 +32,9 @@ async function getCourses() {
 
   const courses = await Course
     // .find({ author: "Mosh", isPublished: true })
-    .find({ price: 10 }) // Find courses that are priced 10 dollars
+    .find({ price: { $gte: 10 } }) // Find courses that are greater than or equal to 10 dollars
+    .find({ price: { $gte: 10, $lte: 20 } }) // Find courses that are greater than or equal to 10 dollars
+    .find({ price: { $in: [10, 15, 20] } }) // Find courses that are 10 or 20 or 15 dollars
     .limit(10)
     .sort({ name: 1 })
     .select({ name: 1, tags: 1 });
