ef0d3a6d4aa45e67f7d2f5a818dae82ec178d6b0
Timi Adewumi <nucleuscaesar@yahoo.com>
2022-03-04 11:30:06 +0100

    Connect to DB, Create Schema, Model and Document

--- index.js
+++ index.js
@@ -3,4 +3,27 @@ const mongoose = require("mongoose");
 mongoose
   .connect("mongodb://localhost/playground")
   .then(() => console.log("Connected to MongoDB..."))
-  .catch((err) => console.log("Could not connect to MongoDB..."));
+  .catch((err) => console.log("Could not connect to MongoDB...", err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+});
+
+async function createCourse() {
+  const Course = mongoose.model("Course", courseSchema);
+  const course = new Course({
+    name: "Angular Course",
+    author: "Mosh",
+    tags: ["angular", "backend"],
+    isPublished: true,
+  });
+
+  const result = await course.save();
+  console.log(result);
+}
+
+createCourse();
