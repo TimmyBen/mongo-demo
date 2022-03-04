6e11b43a34421a681d40c57ae2ff2ce04cd19c93
Timi Adewumi <nucleuscaesar@yahoo.com>
2022-03-04 13:20:08 +0100

    Plain Find Logical Operators

--- index.js
+++ index.js
@@ -28,13 +28,13 @@ async function createCourse() {
 }
 
 async function getCourses() {
-  // eq ne gt gte lt lte in nin
+  // or and
 
   const courses = await Course
     // .find({ author: "Mosh", isPublished: true })
-    .find({ price: { $gte: 10 } }) // Find courses that are greater than or equal to 10 dollars
-    .find({ price: { $gte: 10, $lte: 20 } }) // Find courses that are greater than or equal to 10 dollars
-    .find({ price: { $in: [10, 15, 20] } }) // Find courses that are 10 or 20 or 15 dollars
+    .find()
+    .or([{ author: "Mosh" }, { isPublished: true }]) // Courses that are published or authored by Mosh
+    .and([])
     .limit(10)
     .sort({ name: 1 })
     .select({ name: 1, tags: 1 });
