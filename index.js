79dc9cfebf6ecc653a9208e04ede2e0f5c263cb0
Timi Adewumi <nucleuscaesar@yahoo.com>
2022-03-05 21:25:00 +0100

    Use try block to catch unhandled promise rejection error

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ mongoose
   .catch((err) => console.log("Could not connect to MongoDB...", err));
 
 const courseSchema = new mongoose.Schema({
-  name: String,
+  name: { type: String, required: true },
   author: String,
   tags: [String],
   date: { type: Date, default: Date.now },
@@ -17,34 +17,18 @@ const Course = mongoose.model("Course", courseSchema);
 
 async function createCourse() {
   const course = new Course({
-    name: "Angular Course",
+    // name: "Angular Course",
     author: "Mosh",
     tags: ["angular", "backend"],
     isPublished: true,
   });
 
-  const result = await course.save();
-  console.log(result);
+  try {
+    const result = await course.save();
+    console.log(result);
+  } catch (ex) {
+    console.log(ex.message);
+  }
 }
 
-async function getCourses() {
-  const pageNumber = 2;
-  const pagesize = 10;
-  // /api/courses?pageNumber=2&pageSize=10
-
-  const courses = await Course.find({ author: "Mosh", isPublished: true })
-    .skip((pageNumber - 1) * pageSize)
-    .limit(pageSize)
-    .sort({ name: 1 })
-    .select({ name: 1, tags: 1 });
-  console.log(courses);
-}
-
-async function removeCourse(id) {
-  const result = await Course.deleteOne({ _id: id });
-  // const result = await Course.deleteMany({ _id: id });
-  // const course = await Course.findByIdAndRemove(id);
-  console.log(result);
-}
-
-removeCourse("6221e9c21f1d600422cde622");
+createCourse();
