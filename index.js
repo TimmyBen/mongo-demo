9693f090a899333594c00625c5c36efb92c4266d
Timi Adewumi <nucleuscaesar@yahoo.com>
2022-03-06 23:54:56 +0100

    Built-in Validators for mongoose schema

--- index.js
+++ index.js
@@ -6,18 +6,34 @@ mongoose
   .catch((err) => console.log("Could not connect to MongoDB...", err));
 
 const courseSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: {
+    type: String,
+    required: true,
+    minlength: 255,
+  },
+  category: {
+    type: String,
+    required: true,
+    enum: ["web", "mobile", "network"],
+  },
   author: String,
   tags: [String],
   date: { type: Date, default: Date.now },
   isPublished: Boolean,
+  price: {
+    type: Number,
+    required: function () {
+      return this.isPublished;
+    },
+  },
 });
 
 const Course = mongoose.model("Course", courseSchema);
 
 async function createCourse() {
   const course = new Course({
-    // name: "Angular Course",
+    name: "Angular Course",
+    category: "-",
     author: "Mosh",
     tags: ["angular", "backend"],
     isPublished: true,
