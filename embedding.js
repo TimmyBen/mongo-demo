f23a7608efc984f8a097b4b548b8e3df9824d895
Timi Adewumi <nucleuscaesar@yahoo.com>
2022-03-08 23:32:49 +0100

    Removing an embedded document

--- embedding.js
+++ embedding.js
@@ -40,8 +40,8 @@ async function updateCourse(courseId) {
   const course = await Course.update(
     { _id: courseId },
     {
-      $set: {
-        "author.name": "James Smith",
+      $unset: {
+        author: "",
       },
     }
   );
